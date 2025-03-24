0f48b90229e239a476cbf8284e7cafbfa1f30453
Emily Lundt <110841640+EmilyLundt@users.noreply.github.com>
2025-03-24 14:47:12 -0500

    Update stanfit.R

--- fourmarker/stanfit.R
+++ fourmarker/stanfit.R
@@ -14,7 +14,7 @@ if (any(simdata$id != match(simdata$id, uid))) stop("id is not sequential")
 stan.list <- list(id = simdata$id, y= simdata$y, 
                outcome= as.numeric(simdata$outcome),
                age = simdata$age,
-               x = cbind(simdata$male, simdata$apoe, simdata$apoemiss,
+               x = cbind(simdata$male, simdata$apoepos, simdata$apoemiss,
                          simdata$educ-16),
                adrc = simdata$adrc,
                N = nrow(simdata),  # number of obs
